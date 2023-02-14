d32afce81adffab7afac3d7d880e251ff9c9c65b
Jo-Shino <joe000042@gmail.com>
2023-02-14 21:09:45 +0900

    vercelでAPIが404になるのでnextConfig戻してAPIのURL1つにするとどうなるか検証

--- nft-frontend/front/next.config.js
+++ nft-frontend/front/next.config.js
@@ -5,13 +5,13 @@ const nextConfig = {
 }
 
 const isProd = process.env.NEXT_PUBLIC_DEVELOPMENT;
-const nextEnv = {
-  env: {
-    NEXT_PUBLIC_API_BASE: isProd == "STAGING" ? (
-    process.env.NEXT_PUBLIC_API_URL_STAGING)
-    : (process.env.NEXT_PUBLIC_API_URL_DEV)
-  }
-}
+// const nextEnv = {
+//   env: {
+//     NEXT_PUBLIC_API_BASE: isProd == "STAGING" ? (
+//     process.env.NEXT_PUBLIC_API_URL_STAGING)
+//     : (process.env.NEXT_PUBLIC_API_URL_DEV)
+//   }
+// }
 
 module.exports = nextConfig
-module.exports = nextEnv
+// module.exports = nextEnv
